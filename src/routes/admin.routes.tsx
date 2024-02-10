0c3da66567525b6efcd8038fe6f3acb3c14369a5
Abdullah149081 <masudmd425@gmail.com>
2024-02-10 23:19:01 +0600

    Refactor admin.routes.tsx to use menuitemsGenerator

--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -1,22 +1,10 @@
-import { ReactNode } from 'react';
-import { NavLink } from 'react-router-dom';
 import AdminDashboard from '../pages/admin/AdminDashboard';
 import CreateAdmin from '../pages/admin/CreateAdmin';
 import CreateFaculty from '../pages/admin/CreateFaculty';
 import CreateStudent from '../pages/admin/CreateStudent';
+import { generateMenuItems } from '../utils/menuitems/menuitemsGenerator';
 
-type TRoute = {
-  path: string;
-  element: ReactNode;
-};
-
-type TSidebarItem = {
-  key: string;
-  label: ReactNode;
-  children?: TSidebarItem[];
-};
-
-const adminPaths = [
+export const adminPaths = [
   {
     name: 'Dashboard',
     path: 'dashboard',
@@ -24,6 +12,7 @@ const adminPaths = [
   },
   {
     name: 'User Management',
+    path: 'dashboard',
     children: [
       {
         name: 'Create Admin',
@@ -49,70 +38,4 @@ const adminPaths = [
   },
 ];
 
-export const adminSidebarItems = adminPaths.reduce(
-  (acc: TSidebarItem[], item) => {
-    if (item.path && item.name) {
-      acc.push({
-        key: item.name,
-        label: <NavLink to={`/admin/${item.path}`}>{item.name}</NavLink>,
-      });
-    }
-
-    if (item.children) {
-      acc.push({
-        key: item.name,
-        label: item.name,
-        children: item.children.map((child) => ({
-          key: child.name,
-          label: <NavLink to={`/admin/${child.path}`}>{child.name}</NavLink>,
-        })),
-      });
-    }
-
-    return acc;
-  },
-  []
-);
-
-//* Programatical way
-
-export const adminRoutes = adminPaths.reduce((acc: TRoute[], item) => {
-  if (item.path && item.element) {
-    acc.push({
-      path: item.path,
-      element: item.element,
-    });
-  }
-
-  if (item.children) {
-    item.children.forEach((child) => {
-      acc.push({
-        path: child.path,
-        element: child.element,
-      });
-    });
-  }
-
-  return acc;
-}, []);
-
-//! Hard coded way
-
-// export const adminPaths = [
-//   {
-//     path: 'dashboard',
-//     element: <AdminDashboard />,
-//   },
-//   {
-//     path: 'create-student',
-//     element: <CreateStudent />,
-//   },
-//   {
-//     path: 'create-admin',
-//     element: <CreateAdmin />,
-//   },
-//   {
-//     path: 'create-faculty',
-//     element: <CreateFaculty />,
-//   },
-// ];
+export const adminSidebarItems = generateMenuItems(adminPaths, 'admin');
