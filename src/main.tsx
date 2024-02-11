3af54ef900d9b725dda3ff59be3b74798e016aa8
Abdullah149081 <masudmd425@gmail.com>
2024-02-11 17:48:25 +0600

    Refactor imports in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import { RouterProvider } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import router from './routes/routes.tsx';
-import { store } from './redux/store.ts';
+import { RouterProvider } from 'react-router-dom';
+import './index.css';
+import { store } from './redux/store';
+import router from './routes/routes';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
