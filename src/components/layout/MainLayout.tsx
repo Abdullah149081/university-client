b08d9fff2dcfc8a01d7654b56bbd5c2ef4812d07
Abdullah149081 <masudmd425@gmail.com>
2024-02-11 17:53:07 +0600

    Update import path for Sidebar component

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,6 +1,6 @@
 import { Layout } from 'antd';
 import { Outlet } from 'react-router-dom';
-import Sidebar from './Sidebar';
+import Sidebar from '../ui/Sidebar';
 
 const { Header, Content, Footer } = Layout;
 
