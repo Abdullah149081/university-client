8eb2c07c0c902d69cd0be279020c3483ded6e969
Abdullah149081 <masudmd425@gmail.com>
2024-02-12 11:58:22 +0600

    Add redux-persist and configure store middleware

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,43 @@
 import { configureStore } from '@reduxjs/toolkit';
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+  persistReducer,
+  persistStore,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+import baseApi from './api/baseApi';
+import authReducer from './features/auth/authSlice';
+
+const persistConfig = {
+  key: 'auth',
+  version: 1,
+  storage,
+};
+
+const persistedAuthReducer = persistReducer(persistConfig, authReducer);
+
+const middleware = [baseApi.middleware];
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    [baseApi.reducerPath]: baseApi.reducer,
+    auth: persistedAuthReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(middleware),
 });
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+
 export type AppDispatch = typeof store.dispatch;
+
+export const persisTor = persistStore(store);
