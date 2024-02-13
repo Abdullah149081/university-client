f72e2c7f4e2293f5042c5f3843cdc9252af9069e
Abdullah149081 <masudmd425@gmail.com>
2024-02-13 22:50:47 +0600

    Add logout button to MainLayout

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,15 +1,24 @@
-import { Layout } from 'antd';
+import { Button, Layout } from 'antd';
 import { Outlet } from 'react-router-dom';
+import { logOut } from '../../redux/features/auth/authSlice';
+import { useAppDispatch } from '../../redux/hooks';
 import Sidebar from '../ui/Sidebar';
 
 const { Header, Content, Footer } = Layout;
 
 const MainLayout = () => {
+  const dispatch = useAppDispatch();
+
+  const handlerLogout = () => {
+  dispatch(logOut());
+  };
   return (
     <Layout style={{ height: '100vh' }}>
       <Sidebar />
       <Layout>
-        <Header style={{ padding: 0 }} />
+        <Header style={{ padding: 0 }}>
+          <Button onClick={handlerLogout}>Logout</Button>
+        </Header>
         <Content style={{ margin: '24px 16px 0' }}>
           <div
             style={{
