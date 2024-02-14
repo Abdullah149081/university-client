85dea82fa175d9d7cceb70ec5e07a507b1837447
Abdullah149081 <masudmd425@gmail.com>
2024-02-14 11:59:35 +0600

    Add Toaster component to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
+import { Toaster } from 'sonner';
 import './index.css';
 import { persisTor, store } from './redux/store';
 import router from './routes/routes';
@@ -13,6 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <PersistGate loading={null} persistor={persisTor}>
         <RouterProvider router={router} />
       </PersistGate>
+      <Toaster />
     </Provider>
   </React.StrictMode>
 );
