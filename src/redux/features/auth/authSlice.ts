f09d3f60c666f78e75509f04ce9348d05d18b858
Abdullah149081 <masudmd425@gmail.com>
2024-02-14 12:00:00 +0600

    Update authSlice.ts to include TUser type

--- src/redux/features/auth/authSlice.ts
+++ src/redux/features/auth/authSlice.ts
@@ -2,8 +2,15 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { RootState } from '../../store';
 
+export type TUser = {
+  userId: string;
+  role: string;
+  iat: number;
+  exp: number;
+};
+
 type TAuthState = {
-  user: null | object;
+  user: null | TUser;
   token: null | string;
 };
 
@@ -31,6 +38,6 @@ const authSlice = createSlice({
 export const { setUser, logOut } = authSlice.actions;
 
 export const useCurrentToken = (state: RootState) => state.auth.token;
-export const useCurrentUser = (state: RootState) => state.auth.user;
+export const currentUser = (state: RootState) => state.auth.user;
 
 export default authSlice.reducer;
