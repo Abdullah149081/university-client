05edab0fed6167cee8f93ae667154bec8bc2c790
Abdullah149081 <masudmd425@gmail.com>
2024-02-14 11:59:00 +0600

    Refactor Sidebar component to use currentUser from authSlice

--- src/components/ui/Sidebar.tsx
+++ src/components/ui/Sidebar.tsx
@@ -1,4 +1,6 @@
 import { Layout, Menu } from 'antd';
+import { currentUser } from '../../redux/features/auth/authSlice';
+import { useAppSelector } from '../../redux/hooks';
 import adminPaths from '../../routes/admin.routes';
 import facultyPaths from '../../routes/faculty.routes';
 import studentPaths from '../../routes/student.routes';
@@ -13,10 +15,11 @@ const userRole = {
 };
 
 const Sidebar = () => {
-  const role = 'student';
+  const user = useAppSelector(currentUser);
+
   let sidebarItems;
 
-  switch (role) {
+  switch (user!.role) {
     case userRole.ADMIN:
       sidebarItems = generateMenuItems(adminPaths, userRole.ADMIN);
       break;
