f2e935e2aa42998419bbadb502b28c772f13bee0
Abdullah149081 <masudmd425@gmail.com>
2024-02-14 11:58:41 +0600

    Fix indentation in handlerLogout function

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -10,7 +10,7 @@ const MainLayout = () => {
   const dispatch = useAppDispatch();
 
   const handlerLogout = () => {
-  dispatch(logOut());
+    dispatch(logOut());
   };
   return (
     <Layout style={{ height: '100vh' }}>
