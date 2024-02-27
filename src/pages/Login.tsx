f0a1c5d1ffea714b3ebaee578c8002103ad8e9bc
Abdullah149081 <masudmd425@gmail.com>
2024-02-27 23:08:04 +0600

    Refactor Login page form and add styling

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,33 +1,31 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import { Button } from 'antd';
-import { SubmitHandler, useForm } from 'react-hook-form';
+import { Button, Row } from 'antd';
+import { FieldValues } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'sonner';
+import PHForm from '../components/form/PHForm';
+import PHInput from '../components/form/PHInput';
 import { useLoginMutation } from '../redux/features/auth/authApi';
 import { TUser, setUser } from '../redux/features/auth/authSlice';
 import { useAppDispatch } from '../redux/hooks';
 import verifyToken from '../utils/verifyToken';
 
-type FormValues = {
-  id: string;
-  password: string;
-};
-
 const Login = () => {
-  const { register, handleSubmit } = useForm<FormValues>({
-    defaultValues: { id: 'A-0001', password: 'admin123' },
-  });
-
   const navigate = useNavigate();
   const [login] = useLoginMutation();
   const dispatch = useAppDispatch();
 
-  const onSubmit: SubmitHandler<FormValues> = async (data) => {
+  const defaultValues = {
+    userId: 'A-0001',
+    password: 'admin123',
+  };
+
+  const onSubmit = async (data: FieldValues) => {
     const toastId = toast.loading('Logging in');
 
     try {
       const userInfo = {
-        id: data.id,
+        id: data.userId,
         password: data.password,
       };
 
@@ -41,17 +39,13 @@ const Login = () => {
     }
   };
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div>
-        <label htmlFor="id">ID: </label>
-        <input type="text" id="id" {...register('id')} />
-      </div>
-      <div>
-        <label htmlFor="password">Password: </label>
-        <input type="text" id="password" {...register('password')} />
-      </div>
-      <Button htmlType="submit">Login</Button>
-    </form>
+    <Row justify="center" align="middle" style={{ height: '100vh' }}>
+      <PHForm onSubmit={onSubmit} defaultValues={defaultValues}>
+        <PHInput type="text" name="userId" label="ID:" />
+        <PHInput type="text" name="password" label="Password" />
+        <Button htmlType="submit">Login</Button>
+      </PHForm>
+    </Row>
   );
 };
 
