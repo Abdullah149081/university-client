1e2100df29ae40fd5185d8199d0ddb04ffba6001
Abdullah149081 <masudmd425@gmail.com>
2024-03-25 22:20:22 +0600

    Add form and select components to CreateAcademicSemester component

--- src/pages/admin/academicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -1,8 +1,38 @@
+import { Button, Col, Flex } from 'antd';
+import { FieldValues, SubmitHandler } from 'react-hook-form';
+import PHForm from '../../../components/form/PHForm';
+import PHSelect from '../../../components/form/phSelect';
+import { monthOptions } from '../../../constants/global';
+import { semesterOptions } from '../../../constants/semester';
+
+const currentYear = new Date().getFullYear();
+const yearOptions = [0, 1, 2, 3, 4].map((number) => ({
+  value: String(currentYear + number),
+  label: String(currentYear + number),
+}));
+
 const CreateAcademicSemester = () => {
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    console.log(data);
+  };
+
   return (
-    <div>
-      <h1>This is CreateAcademicSemester component</h1>
-    </div>
+    <Flex justify="center" align="center" >
+      <Col span={6}>
+        <PHForm onSubmit={onSubmit}>
+          <PHSelect label="Name" name="name" options={semesterOptions} />
+          <PHSelect label="Year" name="year" options={yearOptions} />
+          <PHSelect
+            label="Start Month"
+            name="startMonth"
+            options={monthOptions}
+          />
+          <PHSelect label="End Month" name="endMonth" options={monthOptions} />
+
+          <Button htmlType="submit">Submit</Button>
+        </PHForm>
+      </Col>
+    </Flex>
   );
 };
 
