1d8bcfefc3532041faf4055f90ab2f0932c79a8c
Abdullah149081 <masudmd425@gmail.com>
2024-03-28 01:52:16 +0600

    Refactor academic management components and add filtering and pagination to AcademicSemester table

--- src/pages/admin/academicManagement/AcademicSemester.tsx
+++ src/pages/admin/academicManagement/AcademicSemester.tsx
@@ -1,14 +1,129 @@
+import { Button, Table, TableColumnsType, TableProps } from 'antd';
+import { useState } from 'react';
+import { monthNames } from '../../../constants/global';
 import { useGetAllSemestersQuery } from '../../../redux/academicSemester/academicSemesterApi';
+import { TAcademicSemester, TQueryParam } from '../../../types';
+
+type TDataType = Pick<
+  TAcademicSemester,
+  'name' | 'year' | 'startMonth' | 'endMonth'
+>; // Pick the fields that you want to display in the table
 
 const AcademicSemester = () => {
-  const { data } = useGetAllSemestersQuery(undefined);
+  const [param, setParam] = useState<TQueryParam[]>([]);
 
-  console.log(data);
+  const { data: semesterData, isFetching } = useGetAllSemestersQuery(param);
+
+  const tableData = semesterData?.data?.map(
+    ({ _id, name, startMonth, endMonth, year }) => ({
+      key: _id,
+      name,
+      startMonth,
+      endMonth,
+      year,
+    })
+  );
+
+  // Remove duplicate years from the data and create an array of unique years for the filter dropdown options
+  const uniqueYears = Array.from(
+    new Set(tableData?.map((data) => data.year) || [])
+  );
+
+  const yearFilters = uniqueYears.map((year) => ({
+    text: year,
+    value: year,
+  }));
+
+  const monthFilters = monthNames.map((month) => ({
+    text: month,
+    value: month,
+  }));
+
+  const columns: TableColumnsType<TDataType> = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      filters: [
+        {
+          text: 'Autumn',
+          value: 'Autumn',
+        },
+        {
+          text: 'Fall',
+          value: 'Fall',
+        },
+        {
+          text: 'Summer',
+          value: 'Summer',
+        },
+      ],
+    },
+    {
+      title: 'Year',
+      dataIndex: 'year',
+      filters: yearFilters,
+    },
+    {
+      title: 'Start Month',
+      dataIndex: 'startMonth',
+      filters: monthFilters,
+    },
+    {
+      title: 'End Month',
+      dataIndex: 'endMonth',
+      filters: monthFilters,
+    },
+
+    {
+      title: 'Action',
+      key: 'action',
+      render: () => (
+        <div>
+          <Button type="primary">Edit</Button>
+          <Button type="primary" danger style={{ marginLeft: 8 }}>
+            Delete
+          </Button>
+        </div>
+      ),
+    },
+  ];
+
+  const onChange: TableProps<TDataType>['onChange'] = (
+    pagination,
+    filters,
+    sorter,
+    extra
+  ) => {
+    if (extra.action === 'filter') {
+      const filterParams: TQueryParam[] = [];
+
+      filters.name?.forEach((name) => {
+        filterParams.push({ name: 'name', value: name as string });
+      });
+
+      filters.year?.forEach((year) => {
+        filterParams.push({ name: 'year', value: year as string });
+      });
+
+      filters.startMonth?.forEach((startMonth) => {
+        filterParams.push({ name: 'startMonth', value: startMonth as string });
+      });
+
+      filters.endMonth?.forEach((endMonth) => {
+        filterParams.push({ name: 'endMonth', value: endMonth as string });
+      });
+
+      setParam(filterParams);
+    }
+  };
 
   return (
-    <div>
-      <h1> This is AcademicSemester component </h1>
-    </div>
+    <Table
+      loading={isFetching}
+      columns={columns}
+      dataSource={tableData}
+      onChange={onChange}
+    />
   );
 };
 
--- src/pages/admin/academicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -1,9 +1,14 @@
+import { zodResolver } from '@hookform/resolvers/zod';
 import { Button, Col, Flex } from 'antd';
 import { FieldValues, SubmitHandler } from 'react-hook-form';
+import { toast } from 'sonner';
 import PHForm from '../../../components/form/PHForm';
 import PHSelect from '../../../components/form/phSelect';
 import { monthOptions } from '../../../constants/global';
 import { semesterOptions } from '../../../constants/semester';
+import { usePostAcademicSemesterMutation } from '../../../redux/academicSemester/academicSemesterApi';
+import { TResponse } from '../../../types';
+import academicValidateSchema from '../../../validate/academicValidate';
 
 const currentYear = new Date().getFullYear();
 const yearOptions = [0, 1, 2, 3, 4].map((number) => ({
@@ -12,14 +17,47 @@ const yearOptions = [0, 1, 2, 3, 4].map((number) => ({
 }));
 
 const CreateAcademicSemester = () => {
-  const onSubmit: SubmitHandler<FieldValues> = (data) => {
-    console.log(data);
+  const [addAcademicSemester] = usePostAcademicSemesterMutation();
+
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    const toastId = toast.loading('Creating academic semester...');
+
+    const name = semesterOptions[Number(data?.name) - 1]?.label;
+
+    const semesterData = {
+      name,
+      code: data.name,
+      year: data.year,
+      startMonth: data.startMonth,
+      endMonth: data.endMonth,
+    };
+
+    try {
+      const res = (await addAcademicSemester(semesterData)) as TResponse<any>; // Provide the missing type argument for TResponse
+      console.log(res, res.error);
+
+      if (res?.data?.success) {
+        toast.success(res.data.message, {
+          id: toastId,
+        });
+      }
+      if (res?.error) {
+        toast.error(res.error.data.message, {
+          id: toastId,
+        });
+      }
+    } catch (error) {
+      toast.error('Failed to create academic semester', { id: toastId });
+    }
   };
 
   return (
-    <Flex justify="center" align="center" >
+    <Flex justify="center" align="center">
       <Col span={6}>
-        <PHForm onSubmit={onSubmit}>
+        <PHForm
+          onSubmit={onSubmit}
+          resolver={zodResolver(academicValidateSchema)}
+        >
           <PHSelect label="Name" name="name" options={semesterOptions} />
           <PHSelect label="Year" name="year" options={yearOptions} />
           <PHSelect
