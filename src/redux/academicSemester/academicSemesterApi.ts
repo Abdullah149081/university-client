55b98f0ae675a55568e4e7eb6b2950217d42184f
Abdullah149081 <masudmd425@gmail.com>
2024-04-19 15:28:20 +0600

    Add academic management API endpoints

--- src/redux/academicSemester/academicSemesterApi.ts
+++ src/redux/academicSemester/academicSemesterApi.ts
@@ -1,4 +1,10 @@
-import { TAcademicSemester, TQueryParam, TResponseRedux } from '../../types';
+import {
+  TAcademicDepartment,
+  TAcademicFaculty,
+  TAcademicSemester,
+  TQueryParam,
+  TResponseRedux,
+} from '../../types';
 import baseApi from '../api/baseApi';
 
 const academicSemesterApi = baseApi.injectEndpoints({
@@ -35,8 +41,54 @@ const academicSemesterApi = baseApi.injectEndpoints({
         body: data,
       }),
     }),
+
+    //  all academic faculties
+    getAcademicFaculties: builder.query({
+      query: () => {
+        return { url: '/academic-faculties', method: 'GET' };
+      },
+      transformResponse: (response: TResponseRedux<TAcademicFaculty[]>) => {
+        return {
+          data: response.data,
+          meta: response.meta,
+        };
+      },
+    }),
+    addAcademicFaculty: builder.mutation({
+      query: (data) => ({
+        url: '/academic-faculties/create-academic-faculty',
+        method: 'POST',
+        body: data,
+      }),
+    }),
+
+    //  all academic departments
+    getAcademicDepartments: builder.query({
+      query: () => {
+        return { url: '/academic-departments', method: 'GET' };
+      },
+      transformResponse: (response: TResponseRedux<TAcademicDepartment[]>) => {
+        return {
+          data: response.data,
+          meta: response.meta,
+        };
+      },
+    }),
+    addAcademicDepartment: builder.mutation({
+      query: (data) => ({
+        url: '/academic-departments/create-academic-department',
+        method: 'POST',
+        body: data,
+      }),
+    }),
   }),
 });
 
-export const { useGetAllSemestersQuery, usePostAcademicSemesterMutation } =
-  academicSemesterApi;
+export const {
+  useGetAllSemestersQuery,
+  usePostAcademicSemesterMutation,
+  useGetAcademicFacultiesQuery,
+  useAddAcademicFacultyMutation,
+  useGetAcademicDepartmentsQuery,
+  useAddAcademicDepartmentMutation,
+} = academicSemesterApi;
